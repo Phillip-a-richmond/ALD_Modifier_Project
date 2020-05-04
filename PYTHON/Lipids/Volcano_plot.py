3c9e120aa8f3a45f9ee490ae0a1dcc44fb551ef9
franskloet <f.m.vanderkloet@uva.nl>
2020-05-04 18:24:47 +0200

    frans update 1

---
+++ PYTHON/Lipids/Volcano_plot.py
@@ -0,0 +1,17 @@
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import numpy as np
+current_palette = sns.color_palette()
+
+# %%
+lipidData = pd.read_csv('data\lipid_data.csv')
+lipidData.info()
+
+
+# %%
+
+
+
